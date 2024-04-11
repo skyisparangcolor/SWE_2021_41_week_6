4d2d0f7a8f8c7d2ef6532989eac02b26b1b3d2e0
skyisparangcolor <104425412+skyisparangcolor@users.noreply.github.com>
2024-04-11 23:48:12 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-FROM typing import List 
-def twoSum(nums: List[int], target: int) -> List[int]: 
-    #YOUR ANSWER 
-    return 
+from typing import List 
+def twoSum(nums: List[int], target: int): 
+    for i in range (0, len(nums)): 
+        for j in range (i+1, len(nums)): 
+            if nums[i] + nums[j] == target: 
+                return [i,j]
+
+nums = list(map(int, input("num = ").split(',')))
+target = int(input("target = "))
+
+twoSum(nums, target)
